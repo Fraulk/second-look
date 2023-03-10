796c111768ed9b77dcc1a31abc437f0140bf3e9f
f <fratulker@gmail.com>
2023-03-11 00:58:01 +0100

    clean: settings component code and add if statement to show options depending isTodayGallery

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -3,11 +3,13 @@ import { SettingState } from "../../utils/reducer";
 import "./style.scss"
 
 interface SettingsProps {
-    state: SettingState;
+    state: SettingState | any;
     dispatch: Dispatch<{ type: string; payload: any; }>;
 }
 
 export const Settings = ({state, dispatch}: SettingsProps) => {
+    const params = new URLSearchParams(window.location.search);
+    const isTodayGallery = params.get("id") == "873628046194778123"
 
     const handleSettingChange = (e: any) => {
         dispatch({type: e.target.name, payload: e.target.checked})
@@ -18,66 +20,40 @@ export const Settings = ({state, dispatch}: SettingsProps) => {
         dispatch({type: e.target.name, payload: value})
     }
 
+    const settingList = [
+        {name: "markAsSeen", label: 'Show "Mark as seen"', info: 'Hide or show the feature "Mark as seen" so that the shots don\'t get an overlay saying "SEEN"', type: "boolean", showOnlyOnTodaysGallery: true},
+        {name: "linkApp", label: `Link discord ${state.linkApp ? "app" : "web"}`, info: 'Choose which link to use between the discord app version or the discord website version', type: "boolean", showOnlyOnTodaysGallery: false},
+        {name: "scrollToLastSeen", label: 'Scroll to last seen shot"', info: 'Remember your "Marked as seen" shot and scroll to it next time you access the website.', type: "boolean", showOnlyOnTodaysGallery: true},
+        {name: "openLinkClick", label: `${state.openLinkClick ? "Right" : "Left"} click to open links`, info: 'Choose which mouse click opens in fullscreen and which opens the link', type: "boolean", showOnlyOnTodaysGallery: false},
+        {name: "shotCountAtLoad", label: 'Shot count at page load', info: 'Specify how much shot you want to load when opening the website. The lower, the faster site loads', type: "number", showOnlyOnTodaysGallery: true},
+    ]
+
     return (
         <div className="Settings" tabIndex={-1}>
             Settings
-            <div className="setting">
-                <div className="setting-text">
-                    Show "Mark as seen"
-                </div>
-                <div className="setting-info">
-                    Hide or show the feature "Mark as seen" so that the shots don't get an overlay saying "SEEN"
-                </div>
-                <label className="setting-switch">
-                    <input type="checkbox" name="markAsSeen" defaultChecked={state.markAsSeen} onChange={handleSettingChange} />
-                    <span className="slider round"></span>
-                </label>
-            </div>
-            <div className="setting">
-                <div className="setting-text">
-                    Link discord {state.linkApp ? "app" : "web"}
-                </div>
-                <div className="setting-info">
-                    Choose which link to use between the discord app version or the discord website version
-                </div>
-                <label className="setting-switch">
-                    <input type="checkbox" name="linkApp" defaultChecked={state.linkApp} onChange={handleSettingChange} />
-                    <span className="slider round"></span>
-                </label>
-            </div>
-            <div className="setting">
-                <div className="setting-text">
-                    Scroll to last seen shot
-                </div>
-                <div className="setting-info">
-                    Remember your "Marked as seen" shot and scroll to it next time you access the website.
-                </div>
-                <label className="setting-switch">
-                    <input type="checkbox" name="scrollToLastSeen" defaultChecked={state.scrollToLastSeen} onChange={handleSettingChange} />
-                    <span className="slider round"></span>
-                </label>
-            </div>
-            <div className="setting">
-                <div className="setting-text">
-                    {state.openLinkClick ? "Right" : "Left"} click to open links
-                </div>
-                <div className="setting-info">
-                    Choose which mouse click opens in fullscreen and which opens the link
-                </div>
-                <label className="setting-switch">
-                    <input type="checkbox" name="openLinkClick" defaultChecked={!!state.openLinkClick} onChange={handleSettingChange} />
-                    <span className="slider round"></span>
-                </label>
-            </div>
-            <div className="setting">
-                <div className="setting-text">
-                    Shot count at page load
-                </div>
-                <div className="setting-info" style={{bottom: 6}}>
-                    Specify how much shot you want to load when opening the website. The lower, the faster site loads
-                </div>
-                <input type="number" min={10} max={1000} name="shotCountAtLoad" className="setting-number" defaultValue={state.shotCountAtLoad} onChange={handleSettingNumberChange} />
-            </div>
+            {settingList.map((item: {name: string, label: string, info: string, type: string, showOnlyOnTodaysGallery: boolean}, i: number) => (
+                <>
+                {(isTodayGallery || isTodayGallery == false && (isTodayGallery == false && item.showOnlyOnTodaysGallery == false)) &&
+                    <div className="setting">
+                        <div className="setting-text">
+                            {item.label}
+                        </div>
+                        <div className="setting-info" style={i == settingList.length - 1 ? {bottom: 6} : {}}>
+                            {item.info}
+                        </div>
+                        {item.type == "boolean" &&
+                            <label className="setting-switch">
+                                <input type="checkbox" name={item.name} defaultChecked={state[item.name]} onChange={handleSettingChange} />
+                                <span className="slider round"></span>
+                            </label>
+                        }
+                        {item.type == "number" &&
+                            <input type="number" min={10} max={1000} name={item.name} className="setting-number" defaultValue={state[item.name]} onChange={handleSettingNumberChange} />
+                        }
+                    </div>
+                }
+                </>
+            ))}
         </div>
     )
 }
