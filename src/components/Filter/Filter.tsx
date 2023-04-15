4186c5e37d02ea8c2d53d5a30d255551d1b9de68
f <fratulker@gmail.com>
2023-04-15 22:23:42 +0200

    add: better navigation for filter component

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,11 +1,12 @@
 import { useKeyPress } from "../../utils/hooks"
 import "./style.scss"
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export const Filter = (props: {autocomplete: string[], onFilter: (props: any) => void}) => {
     const {autocomplete, onFilter} = props
     const input = useRef<HTMLInputElement>(null)
     let timer: any = undefined
+    const [focusedIndex, setFocusedIndex] = useState(-1)
 
     const handleFilter = (term: string) => {
         if (term.length <= 1) {
@@ -29,8 +30,23 @@ export const Filter = (props: {autocomplete: string[], onFilter: (props: any) =>
         onFilter("")
     }
 
+    useEffect(() => {
+      setFocusedIndex(-1)
+    }, [autocomplete])
+    
+    const goUpward = () => setFocusedIndex((prev) => (prev > 0) ? prev - 1 : autocomplete.length - 1)
+    const goDownward = () => setFocusedIndex((prev) => (prev < autocomplete.length - 1) ? prev + 1 : 0)
+
     useKeyPress(['k'], "ctrlKey", () => input.current?.focus());
     useKeyPress(['Escape'], "", () => input.current?.blur());
+    
+    useKeyPress(['ArrowUp'], "", () => goUpward());
+    useKeyPress(['Tab'], "shiftKey", () => goUpward());
+
+    useKeyPress(['ArrowDown'], "", () => goDownward());
+    useKeyPress(['Tab'], "", () => goDownward());
+
+    useKeyPress(['Enter'], "", () => handleInputChange(autocomplete[focusedIndex] ?? ""));
 
     return (
         <div className="Filter">
@@ -43,9 +59,25 @@ export const Filter = (props: {autocomplete: string[], onFilter: (props: any) =>
                 </div>
                 {autocomplete.length > 0 && 
                     <div className="autocomplete-search">
-                        {autocomplete && autocomplete.map((item, i) => (
-                            <div className="autocomplete-element" onClick={() => handleInputChange(item)} key={i}>{item}</div>
-                        ))}
+                        <div className="autocomplete-list">
+                            {autocomplete && autocomplete.map((item, i) => (
+                                <div className={`autocomplete-element${focusedIndex == i ? " hasFocus" : ""}`} onClick={() => handleInputChange(item)} key={i}>{item}</div>
+                            ))}
+                        </div>
+                        <div className="navigation-keys">
+                            <div className="separator"></div>
+                            <div className="navigation-content">
+                                <div className="navigation-left-content">
+                                    <span className="key">↓</span> or
+                                    <span className="key">Tab</span>
+                                </div>
+                                <div className="navigation-right-content">
+                                    <span className="key">Shift</span> +
+                                    <span className="key">Tab</span> or
+                                    <span className="key">↑</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 }
                 {input.current?.value && <div className="cross" onClick={handleClearSearch}>✕</div>}
